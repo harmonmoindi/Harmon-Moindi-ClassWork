cff139e7c2312772a63993f7cb37878ff2a164f9
harmonmoindi <harmonmoindi@gmail.com>
2026-06-28 09:19:00 +0300

    JS conditions

---
+++ JS/Intro/Conditions.js
@@ -0,0 +1,37 @@
+/* 
+Conditions => Enable you to control the flow of the code by making a decision
+if, else if, and else
+
+    Syntax:
+if (condition){
+
+}  //if the condition is truthy, it will execute.
+*/
+const age = 40
+if (age > 18) {
+    console.log("Adult")
+}
+
+const name1 = 0
+if (name1 == "John"){
+    console.log("enter")
+}
+
+const salary = 50000
+if (salary > 30000){
+    console.log ("Scenarion 3 runs")
+}
+
+const age2 = 60
+if (age2 < 40){
+    console.log ("Scenario 4 runs")
+}
+
+if (10>20){
+    console.log ("Scenario 5 runs")
+
+}
+
+if (20 > 10){
+    console.log ("Scenario 6 runs")
+}
