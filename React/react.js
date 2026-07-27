33c796f11d3a99d1e29ba617fb2c57c515469030
harmonmoindi <harmonmoindi@gmail.com>
2026-07-27 11:59:43 +0300

    React Vite install

---
+++ React/react.js
@@ -0,0 +1,24 @@
+/* 
+React is a js front-end framework. It's the most popular one and it was developed by meta devs. 
+1. Have a good understanding of js. 
+
+Since react is a framework, it has rules and guidelines; to be followed. 
+What can you build with react:
+    - Web apps.
+    - Mobile apps (React Native).
+
+What you need:
+    - nvm installed. Have node v18 and above. 
+
+Creating A React Application. 
+    - Using react app. (old)
+    - vite (modern and recommended)
+
+Vite:
+    - npx create-vite.
+    - select react application using js. 
+    - to stop the vite application from running use - ctrl+c
+    - to start the vite application use - npm run dev
+
+
+ */
