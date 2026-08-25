2410bb487fad5b610b95b7ce18c8fdaad853e3e6
harmonmoindi <harmonmoindi@gmail.com>
2026-08-25 10:56:21 +0300

    Python Intoduction

---
+++ Python/Intro/Data_type.py
@@ -0,0 +1,21 @@
+#Variables
+x = 50              #Integer
+y = 3.14            #Float
+z = "Hello"         #String
+a = True            #Boolean <True, False>
+b = [1, 2, 3, 4]    #List <Array> Mutable <By Value>
+c = {1, 2, 3}       #set
+d = (1, 2, 3)       #Tuple <list> Immutable
+e = {"key": "value"}#Dictionare <object:js>
+#for dictionary use Bracket notation.
+
+#Determining the types
+#`y is ${}`
+print ("X is", x, "Its type", type(x))
+print (f"Y is {y} its type is {type (y)}")
+print (type(z))
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
+print (type(e))
---
+++ Python/Intro/start.py
@@ -0,0 +1,7 @@
+#console.log is "print" in Python
+x = 34
+print ("X is =", x)
+y = 20
+print ("Y is =", y)
+z = x + y
+print ("Z is =", z)
